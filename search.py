6a06539fab6f9b25ce35ec4d0a08e20f5a129a61
chriswang2468 <xinchao@ualberta.ca>
2015-10-26 22:01:59 -0600

    check booking

--- search.py
+++ search.py
@@ -75,7 +75,14 @@ def find_regular_flight(conString,connection,curs,email,src,dst,dep_date):
     print("Index"+5*" "+"Flightno1"+1*" "+"Flightno2"+1*" "+"src"+7*" "+"dst"+7*" "+"Dep_time"+2*" "+"Arr_time"+2*" "+"layover"+3*" "+"price"+5*" "+"seat1"+5*" "+"seat2"+5*" "+"no_stop")
     for i, a in enumerate(flight):
 		    print(str(i + 1) + (10-len(str(i + 1)))*' ' +a[0] +(10-len(a[0]))*' ' + str(a[1]) + (10-len(str(a[1])))*' ' + str(a[3]) + (10-len(str(a[3])))*' '+ str(a[4]) + (10-len(str(a[4])))*' '+ str(a[5]) + (10-len(str(a[5])))*' '+ str(a[6]) +(10-len(str(a[6])))*' '+ str(a[7]) + (10-len(str(a[7])))*' '+ str(a[8])+(10-len(str(a[8])))*' '+ str(a[9])+(10-len(str(a[9])))*' '+ str(a[10])+(10-len(str(a[10])))*' '+str(a[11]))
-
+    check=input("Do you want to book any flight above? (Y/N) ")
+    check=check.upper()
+    if check=="Y":
+	print(1)
+    elif check=="N":
+	return
+    else:
+	print("Invalid input please try again")
 def find_2stop_flight(conString,connection,curs,email,src,dst,dep_date):
     create_view.search_view(conString,connection,curs)
     query = """select 
@@ -147,3 +154,11 @@ def find_2stop_flight(conString,connection,curs,email,src,dst,dep_date):
     for i, a in enumerate(flight1):
 		    print(str(i + 1) + (10-len(str(i + 1)))*' ' +a[0] +(10-len(a[0]))*' ' + str(a[1]) + (10-len(str(a[1])))*' ' + str(a[2]) + (10-len(str(a[2])))*' '+ str(a[4]) + (10-len(str(a[4])))*' '+ str(a[5]) + (10-len(str(a[5])))*' '+ str(a[6]) +(10-len(str(a[6])))*' '+ str(a[7]) + (10-len(str(a[7])))*' '+ str(a[8])+(10-len(str(a[8])))*' '+ str(a[9])+(10-len(str(a[9])))*' '+ str(a[10])+(10-len(str(a[10])))*' '+str(a[11])+(10-len(str(a[11])))*' '+ str(a[12])++(10-len(str(a[12])))*' '+ str(a[13]))
     
+    check=input("Do you want to book any flight above? (Y/N) ")
+    check=check.upper()
+    if check=="Y":
+	print(1)
+    elif check=="N":
+	return
+    else:
+	print("Invalid input please try again")
